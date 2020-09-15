bfb7e5e966ddd2fba3bf6c5efced793194056a09
pqx0703 <pengquanxin@ones.ai>
2020-09-15 16:35:43 +0800

    fix

--- scripts/generate_tag/sync_job.groovy
+++ scripts/generate_tag/sync_job.groovy
@@ -9,18 +9,12 @@ pipeline {
   stages {
     stage("sync status") {
         steps {
-            withCredentials(
-                bindings: [usernamePassword(credentialsId: 'GITHUB_ACCOUNT_ONES', \
-                            usernameVariable: 'GITHUB_USER', \
-                            passwordVariable: 'GITHUB_ACCESS_TOKEN')
-            ]) {
                 sh 'python --version'
                 sh 'git config --global url."https://${TEMP_GITHUB_TOKEN}:x-oauth-basic@api.github.com/".insteadOf "https://api.github.com/"'
                 sh 'sed "s/TOKEN_IN_JENKINS/${TEMP_GITHUB_TOKEN}/g" ./scripts/generate_tag/scripts/release_config.json > ./scripts/generate_tag/scripts/release_config_use.json '
                 sh 'sed "s/MARS_TOKEN/${MARS_TOKEN}/g" ./scripts/generate_tag/scripts/mars.yaml > ./scripts/generate_tag/scripts/mars_use.yaml '
                 sh "cp ./mars /bin/mars"
                 sh 'python ./scripts/generate_tag/scripts/sync_stable_status.py'
-            }
         }
     }
   }
