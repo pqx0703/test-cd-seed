039b0785e382882cb0a0f446107315ea33ac4964
pqx0703 <pengquanxin@ones.ai>
2020-09-15 16:11:27 +0800

    fix: fix

--- scripts/generate_tag/sync_job.groovy
+++ scripts/generate_tag/sync_job.groovy
@@ -18,6 +18,7 @@ pipeline {
                 sh 'git config --global url."https://${TEMP_GITHUB_TOKEN}:x-oauth-basic@api.github.com/".insteadOf "https://api.github.com/"'
                 sh 'sed "s/TOKEN_IN_JENKINS/${TEMP_GITHUB_TOKEN}/g" ./scripts/generate_tag/scripts/release_config.json > ./scripts/generate_tag/scripts/release_config_use.json '
                 sh 'sed "s/MARS_TOKEN/${MARS_TOKEN}/g" ./scripts/generate_tag/scripts/mars.yaml > ./scripts/generate_tag/scripts/mars_use.yaml '
+                sh "cp ./mars /bin/mars"
                 sh 'python ./scripts/generate_tag/scripts/sync_stable_status.py'
             }
         }
